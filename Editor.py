5e8ee5d4118524f7698e9b00a723dc0e335511fa
Mateusz Ptak <mateusz.ptak@op.pl>
2022-11-09 21:28:40 +0100

    Menu "File" -> "Open" functionality of the editor.
    Create a "Open" existing file action in the "File" menu and its functionality.

--- Editor.py
+++ Editor.py
@@ -1,6 +1,7 @@
 from TableModel import TableModel
-from PyQt5.QtWidgets import QMainWindow, QStatusBar, QTableView, QAction, QMessageBox, QDesktopWidget
+from PyQt5.QtWidgets import QMainWindow, QStatusBar, QTableView, QAction, QMessageBox, QDesktopWidget, QFileDialog
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QUrl
 import csv
 
 
@@ -22,6 +23,12 @@ class Editor(QMainWindow):
         self.NewButton.setStatusTip('Create an empty file.')
         self.NewButton.triggered.connect(self.create_new_file)
         FileMenu.addAction(self.NewButton)
+
+        OpenButton = QAction("&Open", self)
+        OpenButton.setShortcut('Ctrl+O')
+        OpenButton.setStatusTip('Open an existing file.')
+        OpenButton.triggered.connect(self.open_existing_file)
+        FileMenu.addAction(OpenButton)
 
         font = MenuBar.font()
         font.setPointSize(11)
@@ -69,8 +76,8 @@ class Editor(QMainWindow):
         return Reply
 
     def create_new_file(self):
-        question_text = "Do you want to save the actual file before creating a new one?"
-        Reply = self.show_question_message_box(question_text)
+        message_text = "Do you want to save the actual file before creating a new one?"
+        Reply = self.show_question_message_box(message_text)
 
         if Reply == QMessageBox.Yes:
             self.save_current_file()
@@ -83,6 +90,71 @@ class Editor(QMainWindow):
             self.actual_file_name = "unnamed.csv"
             self.setWindowTitle(self.title + self.actual_file_name)
             self.StatusField.showMessage("New file was created.")
+
+    def get_signals_list_as_dictionary(self):
+        return {
+            signal: description
+            for signal, description in self.signals_and_descriptions
+        }
+
+    def show_critical_message_box(self, message_text="Not the right data format!", window_title="Error", button=QMessageBox.Cancel):
+        Reply = QMessageBox.critical(self, window_title, message_text, button)
+        return Reply
+
+    def open_existing_file(self):
+
+        message_text = "Do you want to save the actual file before opening a new one?"
+        Reply = self.show_question_message_box(message_text)
+
+        if Reply == QMessageBox.Yes:
+            self.save_current_file()
+
+        self.file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open existing file",
+            "",
+            "*.csv"
+        )
+
+        if self.file_path:
+
+            Reply = QMessageBox.Ok
+
+            with open(self.file_path, 'r',  encoding="utf-8-sig") as file:
+                opened_file_reader = csv.reader(file, delimiter=";")
+
+                try:
+                    self.signals_and_descriptions = self.define_all_signals_without_descriptions()
+
+                    signals_and_descriptions_dict = self.get_signals_list_as_dictionary()
+
+                    for signal, description in opened_file_reader:
+                        if signal in signals_and_descriptions_dict:
+                            signals_and_descriptions_dict[signal] = description
+                        else:
+                            Reply = self.show_critical_message_box()
+                            break
+
+                    if Reply == QMessageBox.Ok:
+                        self.signals_and_descriptions = [
+                            [signal, signals_and_descriptions_dict[signal]]
+                            for signal in signals_and_descriptions_dict
+                        ]
+
+                        self.actual_file_name = QUrl.fromLocalFile(
+                            self.file_path).fileName()
+                        self.setWindowTitle(self.title + self.actual_file_name)
+
+                except ValueError:
+
+                    Reply = self.show_critical_message_box()
+
+                if Reply == QMessageBox.Ok:
+                    self.refresh_table_view()
+                    message = "Existing file called {} was opened."
+                    self.StatusField.showMessage(
+                        message.format(self.actual_file_name)
+                    )
 
     def save_current_file(self):
         pass
