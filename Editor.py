5dc1739fd1357e94151b65f9c3b6808c61005cd8
Mateusz Ptak <mateusz.ptak@op.pl>
2022-11-09 13:00:02 +0100

    Menu "File" -> "New" functionality of the editor.
    Create a button "File" in the menu bar and a "New" action with its functionality.

--- Editor.py
+++ Editor.py
@@ -1,5 +1,5 @@
 from TableModel import TableModel
-from PyQt5.QtWidgets import QMainWindow, QStatusBar, QTableView, QDesktopWidget
+from PyQt5.QtWidgets import QMainWindow, QStatusBar, QTableView, QAction, QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QIcon
 import csv
 
@@ -10,12 +10,18 @@ class Editor(QMainWindow):
         super().__init__()
 
         self.signals_and_descriptions = self.define_all_signals_without_descriptions()
-
         self.interface()
 
     def interface(self):
-
         MenuBar = self.menuBar()
+
+        FileMenu = MenuBar.addMenu("&File")
+
+        self.NewButton = QAction("&New", self)
+        self.NewButton.setShortcut('Ctrl+N')
+        self.NewButton.setStatusTip('Create an empty file.')
+        self.NewButton.triggered.connect(self.create_new_file)
+        FileMenu.addAction(self.NewButton)
 
         font = MenuBar.font()
         font.setPointSize(11)
@@ -48,9 +54,35 @@ class Editor(QMainWindow):
         self.move(qr.topLeft())
 
     def define_all_signals_without_descriptions(self):
-
         config_file_path = "config/config.csv"
 
         with open(config_file_path, "r", encoding="utf-8-sig") as file:
             fileReader = csv.reader(file, delimiter=";")
             return [[signal, ""] for signal, _ in fileReader]
+
+    def refresh_table_view(self):
+        self.Model = TableModel(self.signals_and_descriptions, self.headerdata)
+        self.Table.setModel(self.Model)
+
+    def show_question_message_box(self, message_text, window_title="Question", buttons=QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel):
+        Reply = QMessageBox.question(self, window_title, message_text, buttons)
+        return Reply
+
+    def create_new_file(self):
+        question_text = "Do you want to save the actual file before creating a new one?"
+        Reply = self.show_question_message_box(question_text)
+
+        if Reply == QMessageBox.Yes:
+            self.save_current_file()
+
+        if Reply == QMessageBox.Yes or Reply == QMessageBox.No:
+            self.signals_and_descriptions = self.define_all_signals_without_descriptions()
+
+            self.refresh_table_view()
+
+            self.actual_file_name = "unnamed.csv"
+            self.setWindowTitle(self.title + self.actual_file_name)
+            self.StatusField.showMessage("New file was created.")
+
+    def save_current_file(self):
+        pass
